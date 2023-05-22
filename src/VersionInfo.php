f3a54985519cb42043c313b1b32f2a5cebbfd09a
NeroLuna <truetone.kro.kr@gmail.com>
2023-05-23 07:54:53 +0900

    [Luna X] 2.0.0-BETA 13 Update!

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -38,7 +38,7 @@ final class VersionInfo{
 
 	public const CLIENT_NAME = "Luna X";
 	public const CLIENT_TYPE = "Ultimate";
-	public const CLIENT_VERSION = "2.0.0-BETA12";
+	public const CLIENT_VERSION = "2.0.0-BETA13";
 
 	private function __construct(){
 		//NOOP
