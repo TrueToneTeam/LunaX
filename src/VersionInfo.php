11e5147fab30dbfecc1813065ae813f7501decd2
NeroLuna <truetone.kro.kr@gmail.com>
2023-07-19 13:33:06 +0900

    Luna X Development build false..

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -57,7 +57,7 @@ final class VersionInfo{
 	public const CLIENT_NAME = "Luna X";
 	public const CLIENT_TYPE = "Ultimate";
 	public const CLIENT_VERSION = "2.3.2";
-	public const CLIENT_IS_DEVELOPMENT_BUILD = true;
+	public const CLIENT_IS_DEVELOPMENT_BUILD = false;
 
 	private function __construct(){
 		//NOOP
